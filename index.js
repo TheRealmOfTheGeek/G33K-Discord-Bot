5794efe3b3f94f600b5cf5761d4d7c73f416b0ee
Kenton Vizdos <kvizdos@gmail.com>
2017-04-19 20:19:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -60,9 +60,8 @@ client.on('message', msg => {
 		msg.reply("Hello! If you need help, please type 'geek help'.");
 	} else if(arg1 == "introduce") {
 		if(arg2 == null) {
-			msg.channel.sendMessage({
-				"Introduce whome? Myself or Kento? (syntax: G33k introduce <yourself/kento/nick)"
-				tts: true});	
+			msg.sendMessage({ to: "", message: "Hello World", tts: false });
+			//msg.channel.sendMessage({"Introduce whome? Myself or Kento? (syntax: G33k introduce <yourself/kento/nick)"tts: true});	
 		} else 	if(arg2 == "yourself") {
 			msg.channel.sendMessage("", {embed: {
 			  color: 3447003,
