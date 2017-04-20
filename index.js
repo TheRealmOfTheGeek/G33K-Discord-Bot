cb2ea43e11d2a2c859ac48b5a0ab053dec933693
Kenton Vizdos <kvizdos@gmail.com>
2017-04-19 20:18:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ client.on('message', msg => {
 	} else if(arg1 == "introduce") {
 		if(arg2 == null) {
 			msg.channel.sendMessage({
-				"Introduce whome? Myself or Kento? (syntax: G33k introduce <yourself/kento/nick)",
+				"Introduce whome? Myself or Kento? (syntax: G33k introduce <yourself/kento/nick)"
 				tts: true});	
 		} else 	if(arg2 == "yourself") {
 			msg.channel.sendMessage("", {embed: {
